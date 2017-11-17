c62e0ef347259c85e09c9ae02dd5eb7fe40e0dc8
anakorn <alexnakorn@gmail.com>
2017-11-17 01:01:28 -0800

    fix always outputting usage info

--- bin/main.js
+++ bin/main.js
@@ -7,29 +7,37 @@ const program = require('commander');
 program
     .version('1.0.0')
     .usage('<script>')
+    .arguments('<script>')
+    .action(main)
     .parse(process.argv);
 
-const script = program.script;
-
-if (typeof script === 'undefined') {
+if (typeof theScript === 'undefined') {
     program.outputHelp();
     process.exit(1);
 }
 
-(async function exec() {
-    const raw = await require('get-stdin')();
-    let input;
+async function main(script) {
+    theScript = script;
     try {
-        input = JSON.parse(raw);
-        const context = vm.createContext({
-            _: ramda,
-        });
-        const fn = vm.runInContext(script, context);
-        if (typeof fn !== 'function') {
-            throw new Error('script must return function');
+        let input;
+        if (false) {
+            // TODO: if stream, concat chunks
+        } else {
+            input = await require('get-stdin')();
         }
-        console.log(fn(input));
+        console.log(invokeScript(script, JSON.parse(input)));
     } catch (e) {
         console.error(e);
     }
-})();
+}
+
+function invokeScript(script, input) {
+    const context = vm.createContext({
+        _: ramda,
+    });
+    const fn = vm.runInContext(script, context);
+    if (typeof fn !== 'function') {
+        throw new Error('script must return function');
+    }
+    return fn(input);
+}
